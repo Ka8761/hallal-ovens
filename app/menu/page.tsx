b46246b28046300d6d5f9dfe1f78fa80d3cdd4cc
ka8761 <dikachinwankwo575@gmail.com>
2026-04-15 11:12:54 +0100

    menu aded better

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -3,7 +3,6 @@ import FoodList from "@/components/FoodList";
 export default function MenuPage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Our Menu</h1>
       <FoodList />
     </div>
   );
